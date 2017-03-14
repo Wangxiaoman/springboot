6721d3f657ad916eadf89b0df0465b2428ead9e8
王晓满 <simmer.wang@uxin.com>
2017-03-14 11:48:02 +0800

    【修改】集成zipkin

--- euraka-service2/src/main/java/com/example/ComputeController.java
+++ euraka-service2/src/main/java/com/example/ComputeController.java
@@ -4,6 +4,9 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.sleuth.Span;
+import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,11 +17,27 @@ public class ComputeController {
     private final Logger logger = Logger.getLogger(getClass());
     @Autowired
     private DiscoveryClient client;
+    @Autowired
+    private Tracer tracer;
+    
     @RequestMapping(value = "/add" ,method = RequestMethod.GET)
     public Integer add(@RequestParam Integer a, @RequestParam Integer b) {
         ServiceInstance instance = client.getLocalServiceInstance();
         Integer r = a + b;
+        Span span = this.tracer.createSpan("http:customTraceEndpoint",new AlwaysSampler());
+        loop();
+        this.tracer.close(span);
         logger.info("/add, host:" + instance.getHost() + ", service_id:" + instance.getServiceId() + ", result:" + r);
         return r;
+    }
+    
+    private void loop(){
+        for(int i=1;i<10;i++){
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
